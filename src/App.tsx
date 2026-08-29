701b905227ac151bf956968fd8065018f1a1f354
Yeseon <yeseon04@ewhain.net>
2026-08-29 12:57:42 +0900

    feat: React 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>React + TypeScript</h1>
+      <p>프로젝트 준비가 완료되었습니다.</p>
+    </main>
+  )
+}
+
+export default App
